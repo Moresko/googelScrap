950401cdc84daa09d88f1944a4009349744788c7
Moresko <46751543+Moresko@users.noreply.github.com>
2025-03-31 14:50:42 +0200

    as

--- google-search-scrapper/src/scraper.py
+++ google-search-scrapper/src/scraper.py
@@ -7,20 +7,26 @@ payload = {
     'parse': True
 }
 
+# Request for the job ID for Google search results.
 response = requests.request(
     method='POST',
     url='https://realtime.oxylabs.io/v1/queries',
-    auth=('Martin_1AbZT','xydWox_1pedpi_jufsab'),
+    auth=('Martin_1AbZT', 'xydWox_1pedpi_jufsab'),
     json=payload
 )
 
+# Extract job ID from response
 job_id = response.json()['results'][0]['job_id']
 
+# Get the first page of results in CSV format.
 response_csv = requests.request(
     method='GET',
-    url=f'http://data.oxylabs.io/v1/queries/{job_id}/results/normalized?format=csv',
-    auth=('Martin_1AbZT','xydWox_1pedpi_jufsab')
+    url=f'http://data.oxylabs.io/v1/queries/{job_id}/results/normalized?format=csv&page=1',  # Assuming page=1 to restrict results to the first page
+    auth=('Martin_1AbZT', 'xydWox_1pedpi_jufsab')
 )
 
+# Save the CSV data to a file
 with open('results.csv', 'wb') as f:
     f.write(response_csv.content)
+
+print('CSV saved as results.csv')
